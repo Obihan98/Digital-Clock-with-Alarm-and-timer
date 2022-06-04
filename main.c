baff53204196573ff9edca211d003527a6fe858e
Orhan Ozbasaran <73716961+Obihan98@users.noreply.github.com>
2022-06-04 14:19:08 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * Digital Clock.c
  *
- * Created: 4/23/2022 10:55:51 AM
+ * Created: 06/04/2022 10:55:51 AM
  * Author : Orhan Ozbasaran
  */ 
 
@@ -33,6 +33,18 @@ void avr_wait(unsigned short msec)
 	TCCR0 = 3;
 	while (msec--) {
 		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
+		SET_BIT(TIFR, TOV0);
+		WDR();
+		while (!GET_BIT(TIFR, TOV0));
+	}
+	TCCR0 = 0;
+}
+
+void avr_wait2(unsigned short msec)
+{
+	TCCR0 = 2;
+	while (msec--) {
+		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 128) * 0.001);
 		SET_BIT(TIFR, TOV0);
 		WDR();
 		while (!GET_BIT(TIFR, TOV0));
@@ -176,6 +188,175 @@ int get_key() {
 	return 0;
 }
 
+// SPEAKER
+typedef enum {
+	A, As, B, C, Cs, D, Ds, Ee, F, Fs, G, Gs, A1, As1, B1, C1, Cs1, D1, Ds1, Ee1, F1, Fs1, G1, Gs1, Z
+} Note;
+
+typedef enum {
+	W, HQ, H, Q, Ei
+} Duration;
+
+typedef struct {
+	Note note;
+	Duration duration;
+} PlayingNote;
+
+void play_note(const PlayingNote* note) {
+	int i, k, wait;
+	double f;
+	k = 0;
+	f = 0;
+	wait = 0;
+	switch (note->note) {
+		case (A1):
+		wait = 46;
+		f = 110;
+		break;
+		case As1:
+		wait = 42;
+		f = 116.54;
+		break;
+		case B1:
+		wait = 40;
+		f = 123.47;
+		break;
+		case C1:
+		wait = 38;
+		f = 130.815;
+		break;
+		case Cs1:
+		wait = 36;
+		f = 138.59;
+		break;
+		case D1:
+		wait = 34;
+		f = 146.83;
+		break;
+		case Ds1:
+		wait = 32;
+		f = 155.565;
+		break;
+		case Ee1:
+		wait = 30;
+		f = 164.815;
+		break;
+		case F1:
+		wait = 29;
+		f = 174.615;
+		break;
+		case Fs1:
+		wait = 28;
+		f = 184.995;
+		break;
+		case G1:
+		wait = 26;
+		f = 196;
+		break;
+		case Gs1:
+		wait = 24;
+		f = 207.65;
+		break;
+		case A:
+		wait = 23;
+		f = 220.00;
+		break;
+		case As:
+		wait = 21;
+		f = 233.08;
+		break;
+		case B:
+		wait = 20;
+		f = 246.94;
+		break;
+		case C:
+		wait = 19;
+		f = 261.63;
+		break;
+		case Cs:
+		wait = 18;
+		f = 277.18;
+		break;
+		case D:
+		wait = 17;
+		f = 293.66;
+		break;
+		case Ds:
+		wait = 16;
+		f = 311.13;
+		break;
+		case Ee:
+		wait = 15;
+		f = 329.63;
+		break;
+		case F:
+		wait = 14;
+		f = 349.23;
+		break;
+		case Fs:
+		wait = 14;
+		f = 369.99;
+		break;
+		case G:
+		wait = 13;
+		f = 392.00;
+		break;
+		case Gs:
+		wait = 12;
+		f = 415.30;
+		break;
+		case Z:
+		wait = 30;
+		f = 166.66;
+		break;
+	}
+	
+	switch (note->duration) {
+		case W:
+		k = f;
+		break;
+		case HQ:
+		k = 0.75 * f;
+		break;
+		case H:
+		k = 0.5 * f;
+		break;
+		case Q:
+		k = 0.25 * f;
+		break;
+		case Ei:
+		k = 0.125 * f;
+		break;
+	}
+	
+	if (wait == 30) {
+		for (i = 0; i < k; i++) {
+			avr_wait2(wait*2);
+		}
+	}
+	else {
+		for (i = 0; i < k; i++) {
+			SET_BIT(PORTB, 3);
+			avr_wait2(wait);
+			CLR_BIT(PORTB, 3);
+			avr_wait2(wait);
+		}
+	}
+
+}
+
+void play_song(const PlayingNote song[], int length) {
+	int i, k;
+	for (i = 0; i < length; i++) {
+		k = get_key();
+		if (k == 2) {
+			break;
+		}
+		play_note(&song[i]);
+	}
+}
+
+// CLOCK
 typedef struct {
 	char *location;
 	int hour;
@@ -219,6 +400,33 @@ void check_time(Time *currentTime) {
 void advance_time(Time *currentTime) {
 	++currentTime->second;
 	check_time(currentTime);
+}
+
+void check_timer(Time *currentTime) {
+	if (currentTime->second >= 60) {
+		currentTime->second = 0;
+		++currentTime->minute;
+	}
+	if (currentTime->minute >= 60) {
+		currentTime->minute = 0;
+		++currentTime->hour;
+	}
+	if (currentTime->second < 0) {
+		currentTime->second = 59;
+		--currentTime->minute;
+	}
+	if (currentTime->minute < 0) {
+		currentTime->minute = 59;
+		--currentTime->hour;
+	}
+	if (currentTime->hour < 0) {
+		currentTime->hour = 0;
+	}
+}
+
+void advance_timer(Time *currentTime) {
+	--currentTime->second;
+	check_timer(currentTime);
 }
 
 // Print clock
@@ -295,9 +503,109 @@ void decrease(int cursor, Time *currentTime) {
 	}
 	check_time(currentTime);
 }
-
+ 
 int main() {
-	Time times[10];
+	SET_BIT(DDRB, 3);
+	PlayingNote star_wars[] = {
+			{G1, HQ},
+			{Z, Q},
+			{G1, HQ},
+			{Z, Q},
+			{G1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, W},
+			{G1, H},
+			{Z, H},
+			{D, HQ},
+			{Z, Q},
+			{D, HQ},
+			{Z, Q},
+			{D, HQ},
+			{Z, Q},
+			{Ds, H},
+			{Z, Q},
+			{As, Q},
+			{Fs1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, HQ},
+			{G1, HQ},
+			{Z, Q},
+			{G1, HQ},
+			{Z, Q},
+			{G1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, W},
+			{G1, H},
+			{Z, H},
+			{D, HQ},
+			{Z, Q},
+			{D, HQ},
+			{Z, Q},
+			{D, HQ},
+			{Z, Q},
+			{Ds, H},
+			{Z, Q},
+			{As, Q},
+			{Fs1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, HQ},
+			{G1, HQ},
+			{Z, Q},
+			{G1, HQ},
+			{Z, Q},
+			{G1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, W},
+			{G1, H},
+			{Z, H},
+			{D, HQ},
+			{Z, Q},
+			{D, HQ},
+			{Z, Q},
+			{D, HQ},
+			{Z, Q},
+			{Ds, H},
+			{Z, Q},
+			{As, Q},
+			{Fs1, HQ},
+			{Z, Q},
+			{Ds1, H},
+			{Z, Q},
+			{As, Q},
+			{G1, HQ},
+			/* Keep going... */
+		};
+	Time times[11];
 	int currentTimeCursor = 0;
 	int currentAlarmCursor = 5;
 	// Initialize 5 world clocks
@@ -312,11 +620,13 @@ int main() {
 	init_time(&times[7], 00, 00, 00, "Alarm 3:            ");
 	init_time(&times[8], 00, 00, 00, "Alarm 4:            ");
 	init_time(&times[9], 00, 00, 00, "Alarm 5:            ");
+	// After this point timer is initialized
+	init_time(&times[10], 00, 00, 10, "Timer:            ");
+	// Mode 0: Alarm Clock, Mode 1: Timer
+	int mode = 0;
 	lcd_init();
 	while (1) 
 	{
-		// Mode 0: Alarm Clock, Mode 1: Timer
-		int mode = 0;
 		// Variable for key press
 		int k, b, c;
 		// Variable for cursor when configuring time
@@ -439,13 +749,54 @@ int main() {
 				times[currentTimeCursor].second == times[j].second){
 					// Play sound
 					while(1){
-						
+						play_song(star_wars, 99);
 					}
 				}
 			}
 		// Timer mode
 		} else if (mode == 1){
-			
+			// Enter Setting mode
+			if (k == 1) {
+				while (1) {
+					b = get_key();
+					// Exit setting mode
+					if (b == 2)
+					{
+						break;
+					}
+					// Move cursor right
+					else if (b == 4)
+					{
+						++cursor;
+						if (cursor == 4) {
+							cursor = 1;
+						}
+					}
+					// Increase current cursor
+					else if (b == 3){
+						increase(cursor, &times[10]);
+					}
+					// Decrease current cursor
+					else if (b == 7){
+						decrease(cursor, &times[10]);
+					}
+					print_empty(cursor, &times[10]);
+					avr_wait(200);
+					print_clock(&times[10]);
+					avr_wait(200);
+				}
+			}
+			// Alarm is off!
+			advance_timer(&times[10]);
+			if (times[10].hour == 0 &&
+				times[10].minute == 0 &&
+				times[10].second == 0){
+					// Play sound
+					while(1){
+						play_song(star_wars, 99);
+					}
+				}
+			print_clock(&times[10]);
 		}
 	}
 	return 0;
